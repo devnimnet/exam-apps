754c594c6580e0cdb2dfcb85b3d9868fde9764fd
Hamida Sonia Dewi <hamidasonia11@gmail.com>
2026-02-24 10:24:21 +0700

    init project

---
+++ android/app/src/main/kotlin/com/example/examp_app/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.examp_app
+
+import android.app.ActivityManager
+import android.app.Activity
+import android.content.Context
+import android.os.Bundle
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val am = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        if (am.lockTaskModeState == ActivityManager.LOCK_TASK_MODE_NONE) {
+            startLockTask()
+        }
+    }
+}
